e330002d8ff02010932a31ec61d785f4bf0180f2
emersonjr1 <emerson.maximojunior@outlook.com.br>
2026-09-02 20:26:29 -0300

    Realizado a alteração do Exercicio01 da semana01 e criado uma pasta Learn

---
+++ LEARN/Program.cs
@@ -0,0 +1,13 @@
+﻿// PACOTE COM RECURSOS = Dentro desse pacote estamos referenciando o "system"
+using System;
+
+namespace csharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
