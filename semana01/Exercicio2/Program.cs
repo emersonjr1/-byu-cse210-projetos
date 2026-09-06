ca7ec8c95a32ff044e9ef1ba1e99856846f43cd4
emersonjr1 <emerson.maximojunior@outlook.com.br>
2026-09-06 00:03:12 -0300

    Realizado os estudos da semana

--- semana01/Exercicio2/Program.cs
+++ semana01/Exercicio2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio2.");
+        Console.Write("Qual a nota numérica ");
+        int nota = int.Parse(Console.ReadLine());
+
+        if (nota >= 90){
+
+            string conceito = "A";
+            Console.Write($"Sua é {conceito}; ");
+        } else if (nota >= 80 && nota < 90 ){
+            string conceito = "B";
+            Console.Write($"Sua nota {conceito}");
+        } else if (nota >=70 && nota < 80){
+            string conceito = "C";
+            Console.Write($"Sua nota é {conceito}");
+        }else if (nota >= 60 && nota < 70){
+            string conceito = "D";
+            Console.Write($"Sua nota é ${conceito}");
+        }else
+        {
+            string conceito = "F";
+            Console.Write($"Sua nota é {conceito}");
+        }
+
     }
 }
