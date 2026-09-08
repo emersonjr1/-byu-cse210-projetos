59e6c6b1fb7db4d8a5559d1b3c3ad75dd4fa5fd2
emersonjr1 <emerson.maximojunior@outlook.com.br>
2026-09-07 23:17:26 -0300

    Realizado os exercicios conforme solicitado.

--- semana01/Exercicio3/Program.cs
+++ semana01/Exercicio3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio3.");
+        Random geradorAleatorio = new Random();
+        int numero = geradorAleatorio.Next(0, 101);
+        int intNum;
+        
+        do
+        {
+            
+        Console.Write("Descubra o numero que escolhi entre 0 e 100. Digite um numero: ");
+        string respNum = Console.ReadLine();
+        intNum = int.Parse(respNum);
+
+        if (intNum < numero)
+            {
+                Console.Write("O numero correto é MAIOR. \n");
+            }
+        else if (intNum > numero)
+            {
+              Console.WriteLine("O número correto é MENOR.\n");  
+            }
+        } while (intNum != numero);
+        
+        Console.WriteLine("Parabéns! Você acertou o numero!");
     }
 }
--- semana01/Exercicio4/Program.cs
+++ semana01/Exercicio4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio4.");
+
+        Console.WriteLine("\n Insira uma lista de números negativos ou positivos; E digite 0 quando terminar.");
+        List<int> numeros = new List<int>();
+
+        while (true)
+        {
+            Console.WriteLine("Digite um numero: ");
+            string entrada = Console.ReadLine();
+            int num = int.Parse(entrada);
+
+            if (num == 0)
+            {
+                break;
+            }
+
+            numeros.Add(num);
+        }
+
+        int soma = numeros.Sum();
+        int maior = numeros.Max();
+        double media = (double)soma / numeros.Count;
+        
+        Console.Write($"\nA soma é: {soma} ");
+        Console.Write($"\nA média é: {media}");
+        Console.Write($"\nO maior numero é: {maior}");
+        
     }
 }
