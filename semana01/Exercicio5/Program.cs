fd16c40ceeba0848024afdc9cc0714c5063516f7
emersonjr1 <emerson.maximojunior@outlook.com.br>
2026-09-08 19:30:59 -0300

    Realizado os exercicios 4 e 5

--- semana01/Exercicio5/Program.cs
+++ semana01/Exercicio5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
+        static void ExibirBoasVindas(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+        }
+
+        static string PerguntarNomeUsuario(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            string nome = Console.ReadLine();
+            return nome;
+        }
+
+        static int PerguntarNumeroFavorito(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            int numero = int.Parse(Console.ReadLine());
+            return numero;
+        }
+        
+        static int ElevarAoQuadrado(int numero, int expoencial)
+        {
+            
+            int quadrado = (int)Math.Pow(numero, expoencial);
+            // OR numeo * numero
+            return quadrado;
+        }
+
+        static void ExibirResultado(string nome, int quadrado)
+        {
+            Console.WriteLine($"{nome}, o quadrado do seu numero é {quadrado}");
+        }
+
         Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio5.");
+        ExibirBoasVindas("Bem vindo ao programa de cálculo.");
+        string nome = PerguntarNomeUsuario("Qual seu nome de usuario?");
+        int numero = PerguntarNumeroFavorito("Qual seu numero favorito?");
+        int quadrado = ElevarAoQuadrado(numero,2);
+        ExibirResultado(nome,quadrado);
+        
     }
 }
